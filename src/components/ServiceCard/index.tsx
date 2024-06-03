e1bb43389ac1d3a87bfaafbcd5938e7482bbe2eb
leenbassam <leen199xx@gmail.com>
2024-06-03 03:12:51 -0700

    user profile icon

--- src/components/ServiceCard/index.tsx
+++ src/components/ServiceCard/index.tsx
@@ -12,11 +12,7 @@ const ServiceCard = ({ title, imageSrc, status, description, slug }) => {
           <svg className="card__arc" xmlns="http://www.w3.org/2000/svg">
             <path />
           </svg>
-          {/* <img
-              className="card__thumb"
-              src="https://i.imgur.com/7D7I6dI.png"
-              alt=""
-            /> */}
+       
           <div className="card__header-text">
             <h3 className="card__title">{title}</h3>
             <span className="card__status">{status}</span>
--- src/pages/NavBar/index.jsx
+++ src/pages/NavBar/index.jsx
@@ -1,13 +1,42 @@
-import React from "react";
+import React, { useState, useEffect, useRef } from "react";
 import { Link as RouterLink, NavLink } from "react-router-dom";
 import logo from "../../img/logo.png";
 import "./style.css";
 import "../../index.css";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faUser } from "@fortawesome/free-solid-svg-icons";
+import { faUserCircle } from "@fortawesome/free-solid-svg-icons"; // Use faUserCircle for the profile icon
 import { useAuth } from "../../auth.js";
 
-function NavBar({ isLoggedIn, toggleLoginStatus, handleLogout }) {
+function NavBar({ isLoggedIn, handleLogout }) {
+  const [dropdownVisible, setDropdownVisible] = useState(false);
+  const dropdownRef = useRef(null);
+
+  const toggleDropdown = () => {
+    setDropdownVisible(!dropdownVisible);
+  };
+
+  const closeDropdown = () => {
+    setDropdownVisible(false);
+  };
+
+  useEffect(() => {
+    const handleClickOutside = (event) => {
+      if (dropdownRef.current && !dropdownRef.current.contains(event.target)) {
+        closeDropdown();
+      }
+    };
+
+    if (dropdownVisible) {
+      document.addEventListener("mousedown", handleClickOutside);
+    } else {
+      document.removeEventListener("mousedown", handleClickOutside);
+    }
+
+    return () => {
+      document.removeEventListener("mousedown", handleClickOutside);
+    };
+  }, [dropdownVisible]);
+
   return (
     <nav className="nav">
       <div className="routes">
@@ -30,23 +59,37 @@ function NavBar({ isLoggedIn, toggleLoginStatus, handleLogout }) {
               Login
             </RouterLink>
           ) : (
-            <>
-              <FontAwesomeIcon icon={faUser} onClick={toggleLoginStatus} />
-              {isLoggedIn && (
-                <div className="dropdown-menu" style={{ position: "relative" }}>
-                  <button
-                    onClick={handleLogout}
-                    style={{
-                      backgroundColor: "#f8f9fa",
-                      border: "none",
-                      cursor: "pointer",
-                    }}
-                  >
-                    Log Out
-                  </button>
-                </div>
+            <div className="dropdown-container" ref={dropdownRef}>
+              <FontAwesomeIcon
+                icon={faUserCircle}
+                onClick={toggleDropdown}
+                className="user-icon"
+              />
+              {dropdownVisible && (
+                <ul className="dropdown-menu">
+                  <li>
+                    <button
+                      onClick={() => {
+                        closeDropdown();
+                        // Add navigation to profile page if needed
+                      }}
+                    >
+                      Profile
+                    </button>
+                  </li>
+                  <li>
+                    <button
+                      onClick={() => {
+                        closeDropdown();
+                        handleLogout();
+                      }}
+                    >
+                      Log Out
+                    </button>
+                  </li>
+                </ul>
               )}
-            </>
+            </div>
           )}
         </div>
       </div>
