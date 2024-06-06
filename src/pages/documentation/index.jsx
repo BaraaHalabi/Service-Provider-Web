5257495a08e52938d6b4f8aaf025d097c5b0fe31
leenbassam <leen199xx@gmail.com>
2024-06-06 05:23:00 -0700

    script page

--- src/pages/documentation/index.jsx
+++ src/pages/documentation/index.jsx
@@ -6,13 +6,12 @@ const DocPage = () => {
   const [userName, setUserName] = useState("");
   const [script, setScript] = useState("");
   const [api, setApi] = useState("");
+  const userId = localStorage.getItem("userID");
+  const token = localStorage.getItem("token");
 
   useEffect(() => {
     const fetchUserData = async () => {
       try {
-        const userId = localStorage.getItem("userID");
-        const token = localStorage.getItem("token");
-
         if (!userId || !token) {
           console.error("User ID or Token is undefined or null");
           return;
@@ -38,7 +37,12 @@ const DocPage = () => {
     const fetchScript = async () => {
       try {
         const response = await axios.get(
-          "http://127.0.0.1:8000/api/generate_script"
+          "http://127.0.0.1:8000/api/generate_script",
+          {
+            headers: {
+              Authorization: `Bearer ${token}`,
+            },
+          }
         );
         setScript(response.data);
       } catch (error) {
@@ -48,7 +52,12 @@ const DocPage = () => {
     const fetchApi = async () => {
       try {
         const response = await axios.get(
-          "http://127.0.0.1:8000/api/generate_api"
+          "http://127.0.0.1:8000/api/generate_api",
+          {
+            headers: {
+              Authorization: `Bearer ${token}`,
+            },
+          }
         );
         setApi(response.data);
       } catch (error) {
@@ -72,15 +81,13 @@ const DocPage = () => {
           <label htmlFor="tab2-1">Script</label>
           <input id="tab2-1" name="tabs-two" type="radio" defaultChecked />
           <div>
-            <h4>Script</h4>
-            <pre>{script}</pre>
+            <p>{script}</p>
           </div>
         </div>
         <div className={styles["tab-2"]}>
           <label htmlFor="tab2-2">API</label>
           <input id="tab2-2" name="tabs-two" type="radio" />
           <div>
-            <h4>API</h4>
             <p>{api}</p>
           </div>
         </div>
