bee3b81377cbc0a945e89d63f059daa13dd07597
leenbassam <leen199xx@gmail.com>
2024-06-13 05:24:52 -0700

    fiv ServiceCard

--- src/components/ServiceCard/index.tsx
+++ src/components/ServiceCard/index.tsx
@@ -2,17 +2,29 @@ import React from "react";
 import { Link } from "react-router-dom";
 import "./style.css";
 
-const ServiceCard = ({ title, imageSrc, status, description, slug }) => {
+interface ServiceCardProps {
+  title: string;
+  imageSrc: string;
+  status: string;
+  description: string;
+  slug: string;
+}
+
+const ServiceCard: React.FC<ServiceCardProps> = ({
+  title,
+  imageSrc,
+  status,
+  description,
+  slug,
+}) => {
   return (
     <Link to={`/services/${slug}`} className="card">
-      {" "}
       <img src={imageSrc} className="card__image" alt={title} />
       <div className="card__overlay">
         <div className="card__header">
           <svg className="card__arc" xmlns="http://www.w3.org/2000/svg">
             <path />
           </svg>
-       
           <div className="card__header-text">
             <h3 className="card__title">{title}</h3>
             <span className="card__status">{status}</span>
