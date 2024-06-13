ae31014d8bfe03d6e8fd6b6eff89234c583958a4
leenbassam <leen199xx@gmail.com>
2024-06-13 05:26:22 -0700

    fix/remove MoreServicesd

--- src/pages/MoreServices/index.tsx
+++ src/pages/MoreServices/index.tsx
@@ -1,31 +1,31 @@
-// MoreServicesPage.js
-import React from "react";
-import ServiceCard from "../../components/ServiceCard/index.tsx";
-import Layout from "../../layout.js";
-import "./style.css";
-import services from "../../components/servicesData.js";
+// // MoreServicesPage.js
+// import React from "react";
+// import ServiceCard from "../../components/ServiceCard/index.tsx";
+// import Layout from "../../layout.js";
+// import "./style.css";
+// import services from "../../components/servicesData.js";
 
-const MoreServicesPage = () => {
-  const limitedServices = services.slice(0, 9);
+// const MoreServicesPage = () => {
+//   const limitedServices = services.slice(0, 9);
 
-  return (
-    <Layout>
-      <div id="more-services">
-        <div className="services-grid">
-          {limitedServices.map((service, index) => (
-            <ServiceCard
-              key={index}
-              title={service.title}
-              imageSrc={service.imgSrc}
-              status={service.status}
-              description={service.description}
-              slug={service.slug}
-            />
-          ))}
-        </div>
-      </div>
-    </Layout>
-  );
-};
+//   return (
+//     <Layout>
+//       <div id="more-services">
+//         <div className="services-grid">
+//           {limitedServices.map((service, index) => (
+//             <ServiceCard
+//               key={index}
+//               title={service.title}
+//               imageSrc={service.imgSrc}
+//               status={service.status}
+//               description={service.description}
+//               slug={service.slug}
+//             />
+//           ))}
+//         </div>
+//       </div>
+//     </Layout>
+//   );
+// };
 
-export default MoreServicesPage;
+// export default MoreServicesPage;
